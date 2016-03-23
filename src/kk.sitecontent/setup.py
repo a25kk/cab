a7368f37cb833b02afc8daa7ee590580bfc05dba
Christoph Böhner-Figas <boehner@team23.de>
2016-03-23 11:13:26 +0100

    Cleanup setup.py dependencies and pep8

--- src/kk.sitecontent/setup.py
+++ src/kk.sitecontent/setup.py
@@ -7,14 +7,14 @@ setup(name='kk.sitecontent',
       version=version,
       description="KK Folderish Sitecontent",
       long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from
       # http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
-        "Framework :: Plone",
-        "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        ],
+          "Framework :: Plone",
+          "Programming Language :: Python",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+      ],
       keywords='',
       author='',
       author_email='',
@@ -36,10 +36,5 @@ setup(name='kk.sitecontent',
       [z3c.autoinclude.plugin]
       target = plone
       """,
-      # The next two lines may be deleted after you no longer need
-      # addcontent support from paster and before you distribute
-      # your package.
-      setup_requires=["PasteScript"],
-      paster_plugins = ["ZopeSkel"],
 
       )
